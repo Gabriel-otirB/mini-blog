403635a049023d085214166c96389a3858adef9e
Gabriel-otirB <lordglayder@gmail.com>
2025-03-12 15:03:13 -0300

    Navbar and Footer structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <main>
+      <Navbar />
       <div className="container">
         <Outlet />
       </div>
+      <Footer />
     </main>
   )
 }
