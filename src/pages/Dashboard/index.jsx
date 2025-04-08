826f0ac76657ceea0b48e652e7eda2ce3565b580
Gabriel-otirB <lordglayder@gmail.com>
2025-04-08 12:06:17 -0300

    feature: Finishing dashboard structure

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -10,7 +10,15 @@ const Dashboard = () => {
   const { user } = useAuthValue();
   const uid = user.uid;
 
-  const {documents: posts, loading} = useFetchDocuments("posts", null, uid);
+  const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
+
+  const deleteDocument = (id) => {
+    
+  }
+
+  if(loading) {
+    return <p>Carregando...</p>;
+  }
 
   return (
     <div>
@@ -24,15 +32,23 @@ const Dashboard = () => {
           </Link>
         </div>
       ) : (
-        <div>
-          Tem posts!
-        </div>
+        <>
+          <div>
+            <span>Título</span>
+            <span>Ações</span>
+          </div>
+
+          {posts && posts.map((post) =>
+            <div key={post.id}>
+              <p>{post.title}</p>
+              <div>
+                <Link to={`/posts/${post.id}`} className="btn btn-outline">Ver</Link>
+                <Link to={`/posts/edit/${post.id}`} className="btn btn-outline">Editar</Link>
+                <button onClick={() => deleteDocument(post.id)} className="btn btn-outline btn-danger">Excluir</button>
+              </div>
+            </div>)}
+        </>
       )}
-
-      {posts && posts.map((post) => (
-        <h3>{post.titlegt}</h3>
-      ))}
-
     </div>
   )
 }
